004fb596d040526d91cc1e6fddb88cb906478d66
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-04-01 13:38:29 +0500

    setup color or statusbar color issues

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context'
+import Colors from '@/constants/color';
+import {  StyleSheet, Text, View } from 'react-native';
+import {StatusBar as ExpoStatusBar} from 'expo-status-bar';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const Index = () => {
   const insets = useSafeAreaInsets();
   return (
-    <View style={{ flex: 1, paddingTop: insets.top, paddingBottom: insets.bottom, paddingLeft: insets.left, paddingRight: insets.right }}>
+    <View style={[styles.container, { paddingTop: insets.top, paddingBottom: insets.bottom, paddingLeft: insets.left, paddingRight: insets.right }]} >
+      <ExpoStatusBar style="light" backgroundColor={Colors.background} />
       <Text>First App</Text>
     </View>
   )
@@ -13,5 +16,9 @@ export default Index
 
 
 const styles = StyleSheet.create({
-
+container: {
+  flex: 1,
+  backgroundColor: Colors.background,
+  color: Colors.textPrimary,
+},
 })
