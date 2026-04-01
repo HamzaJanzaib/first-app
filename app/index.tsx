4d417b14fdf09bd4cc4b173db2f4efd304c5ecea
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-04-01 12:52:38 +0500

    using button component

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,20 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
+        gap: 16,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
       <Text>Hello World.</Text>
+      <Button
+        title="Press me"
+        onPress={() => alert("Button pressed!")}
+      />
     </View>
   );
 }
