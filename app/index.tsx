c9ca3422f874f73f394a808213926466b750d0f7
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-04-01 13:23:30 +0500

    FlatList testig with more data

--- app/index.tsx
+++ app/index.tsx
@@ -3,17 +3,48 @@ import { FlatList, ImageBackground, ScrollView, Text } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function Index() {
-  return (
-    <SafeAreaProvider>
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <FlatList
-          data={[{ key: "1"  }, { key: "2" }, { key: "3" }]}
-          renderItem={({ item }) => (
-           <Text style={{ fontSize: 24, padding: 20 }}>Item {item.key}</Text>
-          )}
-        />
+  const Data = [
+    { key: "1" },
+    { key: "2" },
+    { key: "3" },
+    { key: "4" },
+    { key: "5" },
+    { key: "6" },
+    { key: "7" },
+    { key: "8" },
+    { key: "9" },
+    { key: "10" },
+    {
+      key: "11"
+    },
+    { key: "12"
+    },
+    {
+      key: "13"
+    },
+    {
+      key: "14"
+    },
+    {
+      key: "15"
+    },
 
-      </ScrollView>
+  ]
+  return (
+    <SafeAreaProvider
+    >
+      <FlatList
+        data={Data}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item }) => (
+          <Text
+            style={{ fontSize: 24, padding: 20 }}
+          >
+            Item {item.key}
+          </Text>
+        )}
+      />
+
     </SafeAreaProvider>
   );
 }
