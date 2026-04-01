bdf579b11c78e0055cf2d5e17765d3d7c1e9e9d7
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-04-01 13:50:40 +0500

    creating header

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,34 @@
 import Colors from '@/constants/color';
-import {  StyleSheet, Text, View } from 'react-native';
-import {StatusBar as ExpoStatusBar} from 'expo-status-bar';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { TASKS } from '@/constants/task';
+import Header from '@/components/Header';
 
 const Index = () => {
   const insets = useSafeAreaInsets();
   return (
     <View style={[styles.container, { paddingTop: insets.top, paddingBottom: insets.bottom, paddingLeft: insets.left, paddingRight: insets.right }]} >
       <ExpoStatusBar style="light" backgroundColor={Colors.background} />
-      <Text>First App</Text>
+
+      <FlatList
+        data={TASKS}
+        keyExtractor={(item) => item.id}
+        ListHeaderComponent={
+          <>
+            {/* header */}
+            <Header />
+            {/* date selector */}
+            {/* filters tabs */}
+
+          </>
+        }
+        contentContainerStyle={styles.list}
+        renderItem={({ item }) => <Text >
+          {item.title}
+        </Text>}
+      />
+
     </View>
   )
 }
@@ -16,9 +36,12 @@ export default Index
 
 
 const styles = StyleSheet.create({
-container: {
-  flex: 1,
-  backgroundColor: Colors.background,
-  color: Colors.textPrimary,
-},
+  container: {
+    flex: 1,
+    backgroundColor: Colors.background,
+    color: Colors.textPrimary,
+  },
+  list: {
+    padding: 16,
+  }
 })
