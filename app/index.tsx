b6d350f5ed43f4cf4253781a6f6eb9cc2f1296f2
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-04-01 13:04:22 +0500

    Pressable

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,18 @@
 import React from "react";
-import { ScrollView, TextInput } from "react-native";
+import { Pressable, ScrollView, TextInput } from "react-native";
 
 export default function Index() {
   const [text, setText] = React.useState("");
   return (
     <ScrollView contentInsetAdjustmentBehavior="automatic"
     >
-    <TextInput
-      keyboardType="name-phone-pad"
-      placeholder="Enter text here..."
-      value={text}
-      onChangeText={(newText) => setText(newText)}
-      multiline
-      numberOfLines={3}
-      style={{
-        height: 80,
-        borderColor: "gray",
-        borderWidth: 1,
-        margin: 10,
-        padding: 10,
-        borderRadius: 12,
-      }}
-    />
+    <Pressable onPress={() => alert("Hello world!")}>
+      <TextInput
+        value={text}
+        onChange={(e) => setText(e.nativeEvent.text)}
+        placeholder="Type something"
+      />
+    </Pressable>
 
     </ScrollView>
   );
