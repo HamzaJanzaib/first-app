bf6255c6801fbb9ac5a7af70bfab157e390712e3
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-04-01 13:59:44 +0500

    creating todo ap pages

--- app/index.tsx
+++ app/index.tsx
@@ -2,11 +2,16 @@ import Colors from '@/constants/color';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { TASKS } from '@/constants/task';
+import { FilterOptions, TASKS } from '@/constants/task';
 import Header from '@/components/Header';
+import DateSelector from '@/components/DateSelector';
+import React from 'react';
+import FilterTabs from '@/components/FilterTabs';
+import TaskCard from '@/components/TaskCard';
 
 const Index = () => {
   const insets = useSafeAreaInsets();
+  const [selectedFilter, setSelectedFilter] = React.useState<FilterOptions>('All');
   return (
     <View style={[styles.container, { paddingTop: insets.top, paddingBottom: insets.bottom, paddingLeft: insets.left, paddingRight: insets.right }]} >
       <ExpoStatusBar style="light" backgroundColor={Colors.background} />
@@ -19,14 +24,14 @@ const Index = () => {
             {/* header */}
             <Header />
             {/* date selector */}
+            <DateSelector />
             {/* filters tabs */}
+            <FilterTabs selected={selectedFilter} onSelect={setSelectedFilter} />
 
           </>
         }
         contentContainerStyle={styles.list}
-        renderItem={({ item }) => <Text >
-          {item.title}
-        </Text>}
+        renderItem={({ item }) => <TaskCard task={item} />}
       />
 
     </View>
@@ -42,6 +47,6 @@ const styles = StyleSheet.create({
     color: Colors.textPrimary,
   },
   list: {
-    padding: 16,
+    padding: 12,
   }
 })
